45b5e4511e5516c3fe57dba7824ddd3df01cce64
quinn zite <quinnzite@gmail.com>
2019-11-05 16:15:19 +0800

    events workflow

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,16 +2,30 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def show
+    @booking = EventBooking.new
   end
 
   def create
     @event = Event.new(event_params)
+    @event.hub = Hub.find(params[:hub_id])
+    @event.user = current_user
+    if @event.save
+      redirect_to hubs_path(@hub)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @event.update(event_params)
+    if @event.save
+      redirect_to hubs_path(@hub)
+    else
+      render :new
+    end
   end
 
   def destroy
