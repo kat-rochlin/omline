3626c5ee670a1d03e75ddb91a10d1fec92d1d04f
quinn zite <quinnzite@gmail.com>
2019-11-05 18:09:46 +0800

    validations

--- app/models/event_booking.rb
+++ app/models/event_booking.rb
@@ -1,5 +1,5 @@
 class EventBooking < ApplicationRecord
   belongs_to :event
   belongs_to :user
-
+  validates :user, uniqueness: { scope: :event, message: "You can only book one spot per event" }
 end
