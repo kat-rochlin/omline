cf00a77c4baeaf6fbda79db9f386e6cffebe4a46
Mason Snyder <msnyder193@gmail.com>
2019-11-07 17:34:46 +0800

    chat room working as well as routing to new chat room

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'user_connections/create'
   get 'teachers_certifications/create'
   get 'teachers_certifications/destroy'
   get 'teachers/new_profile'
@@ -26,11 +25,9 @@ Rails.application.routes.draw do
   get "dashboard", to: "pages#dashboard"
   get "new_hub", to: "pages#current_hub"
 
-  resources :user_connections, only: [:new, :create, :index, :show, :destroy] do
-    resources :messages, only: [ :index, :create ]
+  resources :user_connections do
+    resources :messages
   end
-
-  resources :user_connections, only: :create
 
   resources :events, only: [:edit, :destroy, :update, :show] do
     resources :event_bookings, only: :create
