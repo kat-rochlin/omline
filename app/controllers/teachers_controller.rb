e02deb1a920cfc2bd741cd71c8de3d2df4bb8ef4
Sneha Madlani <sneha.madlani@gmail.com>
2019-11-12 00:47:19 +0800

    teahcers link to profile

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -3,20 +3,22 @@ class TeachersController < ApplicationController
   end
 
   def save_profile
-    params.require(:teacher).permit(:certifications).each do |id|
+    params[:teacher][:certifications].filter { |cert| cert != '' }.each do |id|
       TeacherCertification.create(certification_id: id, user: current_user)
     end
 
-    params.require(:teacher).permit(:styles).each do |id|
+    params[:teacher][:styles].filter { |cert| cert != '' }.each do |id|
       TeacherTeachingStyle.create(teaching_style_id: id, user: current_user)
     end
 
-    params.require(:teacher).permit(:languages).each do |id|
+    params[:teacher][:languages].filter { |cert| cert != '' }.each do |id|
       TeacherLanguage.create(language_id: id, user: current_user)
     end
 
-    params.require(:experience).permit(:studio_name, :location, :description, :start_date, :end_date).each do
-      Experience.create(studio: studio_name, location: location, description: description, start: start_date, end: end_date)
+    params[:experience].each do |experience|
+      experience[1].each do |exp|
+        TeacherExperience.create!(studio_name: exp[1], location: exp[1], description: exp[1], start_date: exp[1], end_date: exp[1], user: current_user)
+      end
     end
 
     redirect_to hubs_path
