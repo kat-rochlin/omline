02c7029a4c9c97452de511c3328dd77ebf537a89
quinn zite <quinnzite@gmail.com>
2019-11-11 12:16:15 +0800

    tags stuff onboarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,24 +7,22 @@ class UsersController < ApplicationController
   end
 
   def update_profile
-   @user = current_user
+    @user = current_user
 
-   if @user.update(user_params)
-    if @user.is_teacher
-      redirect_to new_teacher_profile_path
+    if @user.update(user_params)
+      if @user.is_teacher
+        redirect_to new_teacher_profile_path
+      else
+        redirect_to profile_path(@user.id)
+      end
     else
-      redirect_to profile_path(@user.id)
+      render 'complete_profile'
     end
-   else
-    render 'complete_profile'
-   end
   end
 
   private
 
-
-def user_params
-  params.require(:user).permit(:first_name, :last_name, :birthdate, :nationality, :is_teacher, :profile_photo, :cover_photo)
-end
-
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :birthdate, :nationality, :is_teacher, :profile_photo, :cover_photo)
+  end
 end
