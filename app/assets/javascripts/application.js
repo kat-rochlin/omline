4522c2efe3953666ac0aa0b8b568ca9db3abfe5e
Mason Snyder <msnyder193@gmail.com>
2019-11-12 19:22:35 +0800

    wip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,18 @@
 //= require rails-ujs
 //= require_tree .
+function currentTime() {
+var currentTime = new Date(),
+    hours = currentTime.getHours(),
+    minutes = currentTime.getMinutes();
+    if (minutes < 10) {
+     minutes = "0" + minutes;
+    }
+    return (hours + ":" + minutes);
+}
+function currentDate() {
+  var currentDate = new Date(),
+  day = currentDate.getDate(),
+  month = currentDate.getMonth() + 1,
+  year = currentDate.getFullYear();
+  return (day + "/" + month + "/" + year);
+}
