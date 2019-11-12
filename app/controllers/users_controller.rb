6dbd20d6546906273935139ed25e6517d328d3f6
quinn zite <quinnzite@gmail.com>
2019-11-12 10:20:11 +0800

    fixed tags on onboarding fixed minor dashboard and hub index issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
   end
 
   def update_profile
+    @user = current_user
+
     if params[:save] == "save"
-      @user = current_user
       params[:user][:tag_ids].shift
       params[:user][:tag_ids].each do |tagid|
         newtag = Tag.where(id: tagid)
@@ -17,14 +18,17 @@ class UsersController < ApplicationController
       end
       redirect_to dashboard_path
     else
-      @user = current_user
-
+      params[:user][:tag_ids].shift
+      params[:user][:tag_ids].each do |tagid|
+        newtag = Tag.where(id: tagid)
+        relation = Relationship.new(tag_id: newtag.first.id, tagable: @user)
+        relation.save
+      end
       if @user.update(user_params)
         if @user.is_teacher
           redirect_to new_teacher_profile_path
         else
           redirect_to hubs_path({ :newhub => "newhub" })
-
         end
       else
         render 'complete_profile'
