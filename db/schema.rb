a2833d885308aaa3107f2841b32144a1a7a2c3c1
quinn zite <quinnzite@gmail.com>
2019-11-14 16:55:30 +0800

    message badge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_100508) do
+ActiveRecord::Schema.define(version: 2019_11_14_063923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2019_11_07_100508) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read", default: false
     t.index ["user_connection_id"], name: "index_messages_on_user_connection_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
