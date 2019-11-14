e4c3e84034a25da826295c523f74a4262be3be8e
Sneha Madlani <sneha.madlani@gmail.com>
2019-11-14 15:35:29 +0800

    location name and is teacher concept added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -49,6 +49,6 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:title, :description, :start_time, :end_time, :address, :price, :capacity, :photo)
+    params.require(:event).permit(:title, :description, :start_time, :end_time, :location_name, :address, :price, :capacity, :photo)
   end
 end
